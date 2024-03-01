b0bff3b6d416d035b0067a2206a8b182345e3cde
Martin Ellegård <martin.ellegard@gmail.com>
2024-03-01 13:18:10 +0100

    added wayland things

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,31 +1,63 @@
+set -l os (uname)
+
+
+### PATH/EXPORT ###
 # Clear Path too avoid fish slowing down
 set -e fish_user_paths
-#Set path
-set -U fish_user_paths /Users/martin/.local/bin /Users/martin/.local.bin /Applications /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/libpq/bin /Users/martin/go/bin /Users/martin/.omnisharp/bin /Users/martin/.cargo/bin
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+if test "$os" = Darwin
+    # do things for macOS
+    set -U fish_user_paths /Users/martin/.local/bin /Users/martin/.local.bin /Applications /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/libpq/bin /Users/martin/go/bin /Users/martin/.omnisharp/bin /Users/martin/.cargo/bin
+else if test "$os" = Linux
+    # do things for Linux
+    set -U fish_user_paths $HOME/.local/bin $HOME/.cargo/bin $fish_user_paths
+else
+    # do things for other operating systems
+end
+### PATH END ###
+
+### SET OPTIONS ###
+
 # Supresses fish's intro message
 set fish_greeting
+
 #Set term color
 set TERM xterm-256color
+
 #Set default Editor
 set EDITOR lvim
 
-
-###set manpager
+# Set manpager
 set -x MANPAGER "lvim -c 'set ft=man'"
+### SET OPTIONS END
 
 
-###set keybinding mode
+
+### SET KEYBIND MODE ###
 function fish_user_key_bindings
     #fish_default_key_bindings
     fish_vi_key_bindings
 end
-###
+### SET KEYBIND MODE END ###
 
 ### ALIAS
+
+# App shortcuts
+alias vim='lvim'
+if test "$os" = Darwin
+    # do things for macOS
+    alias hvim="hx"
+else if test "$os" = Linux
+    # do things for Linux
+    alias hvim="helix"
+else
+    # do things for other operating systems
+    alias hvim="helix"
+end
 
 # navigation
 alias ..='cd ..'
@@ -34,13 +66,40 @@ alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
-# Update brew packages
-alias brewup="brew update && brew upgrade"
+## Software update
+if test "$os" = Darwin
+    # do things for macOS
+    alias os-refresh-update="brew update && brew upgrade"   # Update brew pkg db and update packages
+    alias os-update="brew upgrade"                          # Update brew pkg db and update packages
+    alias os-remove="brew remove"                           # Update brew pkg db and update packages
+    alias os-install="brew install"
+    alias os-search="brew search"
+else if test "$os" = Linux
+    # do things for Linux
+    alias os-update-std='sudo pacman -Syu'                  # update only standard pkgs
+    alias os-refresh-update='sudo pacman -Syyu'             # Refresh pkglist & update standard pkgs
+    alias os-update-aur='paru -Sua --noconfirm'             # update only AUR pkgs (paru)
+    alias os-update='paru -Syu --noconfirm'                 # update standard pkgs and AUR pkgs (paru)
+    alias os-unlock='sudo rm /var/lib/pacman/db.lck'        # remove pacman lock
+    alias os-cleanup='sudo pacman -Rns (pacman -Qtdq)'      # remove orphaned packages
+    alias os-install="paru -S"
+    alias os-search="paru -Ss"
+    alias os-remove="paru -Rs"                              # Update brew pkg db and update packages
+else
+    # do things for other operating systems
+end
+# Arch
+# Mac
+## Software update end
 
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
+
+# Tmux
+alias start-work="tmux new-session -s work"
+alias work="tmux"
 
 # confirm before overwriting something
 alias cp="cp -i"
@@ -64,14 +123,8 @@ alias push='git push origin'
 alias tag='git tag'
 alias newtag='git tag -a'
 
-# switch between shells
-# I do not recommend switching default SHELL from bash.
-alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
-alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
-alias tofish="sudo chsh $USER -s /opt/homebrew/bin/fish && echo 'Now log out.'"
-
 # Cloudflared open tunnels
 alias mctunnel="cloudflared access tcp --hostname minecraft.clenchedgaming.com --url localhost:25565"
-alias pgstunnel="cloudflared access tcp --hostname databases.clenchedgaming.com/pgs --url localhost:5434"
+alias pgtunnel="cloudflared access tcp --hostname databases.clenchedgaming.com/pgs --url localhost:5434"
 
 fastfetch
